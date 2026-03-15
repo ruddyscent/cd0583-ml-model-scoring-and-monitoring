b64d30af4d3bc518ed5be43f3d30ee6037d56a4c
Kyungwon Chun <ruddyscent@gmail.com>
2026-03-16 01:08:37 +0900

    fix: update paths and remove deprecated LogisticRegression parameter
    - Updated cron demo file path to match the current Workspace directory structure
    - Removed deprecated `multi_class` parameter from LogisticRegression to ensure compatibility with the latest scikit-learn version used in the Workspace
    - Applied minor formatting improvements for readability and consistency

--- L2/demo1/crondemo.py
+++ L2/demo1/crondemo.py
@@ -1,3 +1,3 @@
-f = open("/home/workspace/L2/demo1/crondemo.txt", "a")
+f = open("/workspace/cd0583-ml-model-scoring-and-monitoring/L2/demo1/crondemo.txt", "a")
 f.write("Your cron job worked - great job!")
 f.close()
--- L2/retraining.py
+++ L2/retraining.py
@@ -3,27 +3,28 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import os
 
-###################Reading Records#############
+################### Reading Records #############
 
 
 
-##################Re-training a Model#############
+################## Re-training a Model #############
 
 
-X=trainingdata.loc[:,['col1','col2']].values.reshape(-1, 2)
-y=trainingdata['col3'].values.reshape(-1, 1)
+X = trainingdata.loc[:,['col1','col2']].values.reshape(-1, 2)
+y = trainingdata['col3'].values.reshape(-1, 1)
 
-logit=LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='auto', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+logit = LogisticRegression(C=1.0, class_weight=None, dual=False, 
+                           fit_intercept=True, intercept_scaling=1, 
+                           l1_ratio=None, max_iter=100, n_jobs=None, 
+                           penalty='l2', random_state=0, solver='liblinear', 
+                           tol=0.0001, verbose=0, warm_start=False)
                     
 
 
 
 
-############Pushing to Production###################
+############ Pushing to Production ###################
+
 
 
 
